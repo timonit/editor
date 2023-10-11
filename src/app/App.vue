da1fb4ee909f7bd1f6a45ed8a4075055985c6fc0
timon <timonsus@gmail.com>
2023-10-11 22:42:18 +0500

    init

---
+++ src/app/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import TextEditor from '@/entities/text-editor.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+    <TextEditor />
+  </div>
+</template>
---
+++ src/entities/text-editor.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { editor } from 'monaco-editor';
+import { ref, onMounted } from 'vue';
+
+
+const editorEl = ref<HTMLElement>();
+
+onMounted(() => {
+  const codeEditor = editor.create(
+    editorEl.value,
+    {
+      value: 'asdasd',
+      language: 'javascript',
+      theme: 'vs-dark',
+      automaticLayout: true,
+      minimap:{
+        enabled: false,
+      },
+      tabCompletion: 'on'
+    }
+  )
+})
+</script>
+
+<template>
+  <div class="editor" ref="editorEl">
+  </div>
+</template>
+
+<style scoped>
+.editor {
+  width: 600px;
+  height: 600px;
+}
+</style>
