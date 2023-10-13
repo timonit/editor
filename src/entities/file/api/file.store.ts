039b850cd82d8bcbf99417f84b0495be8b2a4e16
timon <timonsus@gmail.com>
2023-10-13 21:23:10 +0500

    extract interface in fileStore

--- src/entities/file/api/file.store.ts
+++ src/entities/file/api/file.store.ts
@@ -1,57 +1,6 @@
-import { defineStore, type _GettersTree } from 'pinia'
+import { defineStore } from 'pinia'
 import type { AppFile } from '../types';
-
-interface FileStoreState {
-  files: AppFile[];
-  /**
-   * Идентификатор текущий(открытый) файл
-   */
-  currentFileID?: AppFile['id'];
-}
-
-interface FileStoreGetters extends _GettersTree<FileStoreState> {
-  /**
-   * Возвращает текущий(открытый) файл
-   */
-  getCurrentFile(): AppFile | undefined;
-}
-
-interface FileStoreActions {
-  /**
-   * Добавляет файл с именем.
-   * @param name - Имя файла
-   * @param data - Содержимое файла
-   * @return - Идентификатор файла
-   */
-  createFile(name: string, data?: string): string;
-  /**
-   * Переименовать файл
-   * @param id - Идентификатор файла
-   * @param newName - Новое имя файла
-   */
-  renameFile(id: string, newName: string): void;
-  /**
-   * Удаляет файл
-   * @param id - Идентификатор файла
-   */
-  deleteFile(id: string): void;
-  /**
-   * Изменить содержимое файла
-   * @param id - Идентификатор файла
-   * @param data - Содержимое файла
-   */
-  changeFileData(id: string, data: string): void;
-  /**
-   * Устанавливает идентификатор текущего(открытого) файла
-   * @param id - Идентификатор файла
-   */
-  selectFile(id: string): void;
-  /**
-   * Убирает идентификатор текущего(открытого) файл
-   */
-  clearCurrentFile(): void;
-}
-
+import type { FileStoreState, FileStoreGetters, FileStoreActions } from './types';
 
 export const useFileStore = defineStore<'files', FileStoreState, FileStoreGetters, FileStoreActions>({
   id: 'files',
